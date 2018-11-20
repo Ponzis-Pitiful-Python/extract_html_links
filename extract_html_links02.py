96dbe364dafd3952b8991b7ffff3da3c0f676d7d
Ponzi <43649035+Ponzis-Pitiful-Python@users.noreply.github.com>
2018-11-20 18:56:43 -0500

    Add files via upload

--- extract_html_links02.py
+++ extract_html_links02.py
@@ -11,18 +11,32 @@ url = input("Input url for search: ")
 r = requests.get(url)
 soup = BeautifulSoup(r.content, "html.parser")
 
-theWord = input("Input word you are searching for: ")
 
-da_links = lambda tag: (getattr(tag, 'name', None) == 'a' and
-                           'href' in tag.attrs and
-                           theWord in tag.get_text().lower())
+def word_search():
 
-da_paragraph = lambda tag: (getattr(tag, 'name', None) == 'p' and
-                           theWord in tag.get_text().lower())
+    theWord = input("Input word you are searching for: ")
+    da_links = lambda tag: (getattr(tag, 'name', None) == 'a' and
+                               'href' in tag.attrs and
+                               theWord in tag.get_text().lower())
+    da_paragraph = lambda tag: (getattr(tag, 'name', None) == 'p' and
+                               theWord in tag.get_text().lower())
+    results = soup.find_all(da_links)
+    results2 = soup.find_all(da_paragraph)
 
-results = soup.find_all(da_links)
-results2 = soup.find_all(da_paragraph)
+    from pprint import pprint
+    pprint(results)
+    pprint(results2)
 
-from pprint import pprint
-pprint(results)
-pprint(results2)
+word_search()
+
+print("would you like to run another word search on this url?")
+
+bubba = "y"
+wordFind = input("Type y for another word search, any other letter to quit: ")
+
+while wordFind == bubba:
+    word_search()
+    wordFind = input("Type y for another word search, any other letter to quit: ")
+    if wordFind != bubba:
+        break
+
